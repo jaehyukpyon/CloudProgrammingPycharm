0aa4762b6b116ffd5306d7b4a90ad96eae4a6ac7
jaehyuk <jaehyuk.pyon@gmail.com>
2023-06-05 21:14:40 +0900

    2023/06/05

--- cryptocurrency/views.py
+++ cryptocurrency/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+import pyupbit
 
 # Create your views here.
 
+def get_all_krw_strings():
+  all_list = pyupbit.get_tickers(fiat = "KRW")
+  return all_list
+
 def index(request):
-    pass
+    return render(request, 'crypto/main.html');
