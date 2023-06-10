92ef687da99e1950baaf9a242cfc512503b4b11d
jaehyuk <jaehyuk.pyon@gmail.com>
2023-06-10 15:52:39 +0900

    2023/06/10

--- cryptocurrency/views.py
+++ cryptocurrency/views.py
@@ -8,4 +8,19 @@ def get_all_krw_strings():
   return all_list
 
 def index(request):
-    return render(request, 'crypto/main.html');
+    all_crypto_name_list = get_all_krw_strings()
+    print(all_crypto_name_list)
+
+    crypto_name = request.GET.get("crypto_name")
+    if crypto_name is None:
+        crypto_name = "KRW-BTC"
+
+    print(crypto_name)
+    context = {
+        "crypto_name": crypto_name,
+        "all_crypto_name_list": all_crypto_name_list,
+    }
+    return render(request, 'crypto/main.html', context)
+
+def buy(request):
+    pass
