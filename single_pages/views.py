f72a8ea559ece76bcba60de5cddce85bad2e0fcf
jaehyukpyon <jaehyuk.pyon@gmail.com>
2023-07-10 20:46:17 +0900

    upload uncomitted files

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post
 
 # Create your views here.
 
 def main(request):
-    recent_posts = Post.objects.order_by('-pk')[:3]
-    context = {
-        'recent_posts': recent_posts,
-    }
-    return render(request, 'single_pages/main.html', context)
+    return redirect('/crypto/')
+    # recent_posts = Post.objects.order_by('-pk')[:3]
+    # context = {
+    #     'recent_posts': recent_posts,
+    # }
+    # return render(request, 'single_pages/main.html', context)
 
 
 def test(request, pk):
